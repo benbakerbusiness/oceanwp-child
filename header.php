4e4b7fa90b928225cab41a4a8694f7cab28fb120
jayanthonyjohnwilson <jayanthonyjohnwilson@gmail.com>
2020-01-18 11:39:22 +1000

    1.2.0

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package OceanWP WordPress theme
+ */ ?>
+
+<!DOCTYPE html>
+<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?><?php oceanwp_schema_markup( 'html' ); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <?php do_action('website_before'); ?>
+
+	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+
+	<div id="outer-wrap" class="site clr">
+
+		<?php do_action( 'ocean_before_wrap' ); ?>
+
+		<div id="wrap" class="clr">
+
+			<?php do_action( 'ocean_top_bar' ); ?>
+
+			<?php do_action( 'ocean_header' ); ?>
+            
+            <div>
+                <button class="ocs-trigger ocs-toggle ocs-toggle-left">l</button>
+                <?php get_template_part( 'partials/header/logo' ); ?>
+                <button class="ocs-trigger ocs-toggle ocs-toggle-right">r</button>
+            </div>
+
+			<?php do_action( 'ocean_before_main' ); ?>
+			
+			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?>>
+
+				<?php do_action( 'ocean_page_header' ); ?>
